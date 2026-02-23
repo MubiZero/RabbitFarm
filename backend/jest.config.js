3b05c1e2b0b0add8f31de1fa8a4ad1ed6139a098
mukhamedov.m <mukhamedov.m@eskhata.com>
2026-02-23 09:26:43 +0500

    test: add jest configuration and test helpers

---
+++ backend/jest.config.js
@@ -0,0 +1,11 @@
+module.exports = {
+  testEnvironment: 'node',
+  testMatch: ['**/tests/**/*.test.js'],
+  setupFilesAfterEnv: ['./tests/setup.js'],
+  coverageDirectory: 'coverage',
+  collectCoverageFrom: ['src/**/*.js', '!src/seeders/**'],
+  coverageThreshold: {
+    global: { branches: 70, functions: 80, lines: 80, statements: 80 }
+  },
+  testTimeout: 10000
+};
